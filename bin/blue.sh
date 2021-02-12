0f049bfc258c0c90a4ade6812031cb1aedab0d6b
mats cronqvist <masse@cronqvi.st>
2021-02-12 12:29:36 +0100

    bluer

--- bin/blue.sh
+++ bin/blue.sh
@@ -18,7 +18,13 @@ _connect() {
 }
 
 _info() {
-    (bluetoothctl info 2>/dev/null || true) | grep -E "Name:|Paired:|Connected:" || true
+    for d in $(bluetoothctl devices | awk '{print $2}')
+    do bluetoothctl info "$d" |\
+            awk -F":" '$1 ~ /Name/{gsub(/^ +| +$/,"",$2);name=$2}
+                       $1 ~ /Connected/{gsub(/^ +| +$/,"",$2);conn=$2}
+                       $1 ~ /Paired/{gsub(/^ +| +$/,"",$2);pair=$2}
+                       END{if(name != "")print "{\"name\":\""name"\",\"connected\":\""conn"\",\"paired\":\""pair"\"}"}'
+    done
 }
 
 _pair() {
@@ -34,23 +40,26 @@ _paired() {
     bluetoothctl paired-devices | cut -c25-
 }
 
+_connected() {
+    _info | jq '.|select(.connected == "yes")'
+}
+
 _scan() {
     bluetoothctl --timeout 7 scan on >/dev/null
     if [ -z "$1" ]; then
-        for D in $(bluetoothctl devices | awk '{print $2}')
-        do bluetoothctl info "$D" | grep Name
-        done
+        _info
     else
         bluetoothctl devices | grep "$1" | awk '{print $2}'
     fi
 }
 
 case ${1:-""} in
-    scan) _scan "${2:-""}";;
-    pair) _pair "${2:-""}";;
-    connect) _connect "${2:-""}";;
-    paired) _paired;;
-    info) _info;;
-    restart) _restart;;
-    *) _usage;;
+    scan)      _scan "${2:-""}";;
+    pair)      _pair "${2:-""}";;
+    connect)   _connect "${2:-""}";;
+    connected) _connected;;
+    paired)    _paired;;
+    info)      _info;;
+    restart)   _restart;;
+    *)         _usage;;
 esac
