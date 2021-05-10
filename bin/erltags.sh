378ac0c7739d6366830eaf92def1ff4f94c668e2
mats cronqvist <masse@cronqvi.st>
2021-05-10 10:41:49 +0200

    erltags

--- bin/erltags.sh
+++ bin/erltags.sh
@@ -2,15 +2,22 @@
 
 set -euo pipefail
 
+err() {
+    echo "$1:-"""
+    exit 4
+}
+
 OTP=~/git/otp
-ERL=~/git/loltel/connectivity/erlang
+ERL="$(readlink -f "${1:-~/git/loltel/connectivity/erlang}")"
+
+[ -f "$ERL/rebar.config" ] || err "not a rebar project: $1"
 
 cd "$OTP"
 rm -f TAGS
 for P in lib/*/{src,include} erts/preloaded
 do find "$P" -name "*.[he]rl" -print
 done | etags -
-echo "tagged $OTP"
+echo "etagged $OTP"
 
 cd "$ERL"
 rebar3 compile || true
@@ -18,4 +25,4 @@ rm -f TAGS
 for P in src include apps/* _build/default/lib/*
 do [ -d "$P" ] && find "$P" -name "*.[he]rl" -print
 done | etags -i "$OTP"/TAGS -
-echo "tagged $ERL"
+echo "etagged $ERL"
