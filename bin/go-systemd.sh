9e13fc260d2c408fc0a31f2dcf3f7a5800e325ff
mats cronqvist <masse@cronqvi.st>
2021-10-25 17:33:24 +0200

    slice of...

--- bin/go-systemd.sh
+++ bin/go-systemd.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 # uninstall classic networking
 sudo systemctl daemon-reload
 for u in ifupdown dhcpcd dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
-do systemctl list-units | grep -q "$u.service" && systemctl is-enabled "$u" && sudo systemctl disable --now "$u"
+do systemctl list-units | grep -q "$u.service" &&
+        systemctl is-enabled "$u" &&
+        sudo systemctl disable --now "$u"
 done
-sudo apt --autoremove purge -y ifupdown dhcpcd dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
+sudo apt --autoremove purge -y \
+     ifupdown dhcpcd dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
 sudo rm -rf /etc/network /etc/dhcp
 
 # setup/enable systemd-resolved and systemd-networkd
 for u in avahi-daemon libnss-mdns
-do systemctl list-units | grep -q "$u.service" && systemctl is-enabled "$u" && sudo systemctl disable --now "$u"
+do systemctl list-units | grep -q "$u.service" &&
+        systemctl is-enabled "$u" &&
+        sudo systemctl disable --now "$u"
 done
 sudo apt install -y libnss-resolve
 sudo apt purge -y avahi-daemon libnss-mdns
@@ -21,7 +26,9 @@ sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sudo systemctl enable systemd-networkd.service systemd-resolved.service
 
 # mark these so APT don't reinstall them
-sudo apt-mark hold avahi-daemon dhcpcd dhcpcd5 ifupdown isc-dhcp-client isc-dhcp-common libnss-mdns openresolv raspberrypi-net-mods rsyslog
+sudo apt-mark hold \
+     avahi-daemon dhcpcd dhcpcd5 ifupdown isc-dhcp-client isc-dhcp-common \
+     libnss-mdns openresolv raspberrypi-net-mods rsyslog
 
 # describe wired network
 sudo tee /etc/systemd/network/04-wired.network <<HERE
@@ -37,13 +44,14 @@ HERE
 
 # initialize wpa_supplicant
 for i in $(iw dev | grep -Eo "Interface [a-zA-Z0-0_-]+" | cut -c11-)
-do sudo tee /etc/wpa_supplicant/wpa_supplicant-$i.conf <<HERE
+do I="/etc/wpa_supplicant/wpa_supplicant-$i.conf"
+   sudo tee "$I" <<HERE
 country=GI
 ctrl_interface=DIR=/run/wpa_supplicant GROUP=netdev
 update_config=1
 p2p_disabled=1
 HERE
-   sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant-$i.conf
+   sudo chmod 600 "$I"
 done
 sudo systemctl disable wpa_supplicant.service
 sudo systemctl enable wpa_supplicant@wlan0.service
